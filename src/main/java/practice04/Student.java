03af0ccfe065cc0d8b7bb72c01168fdf5dfeb5d1
wanghc00 <306093226@qq.com>
2019-08-22 22:34:15 +0800

    practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,30 @@
 package practice04;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private int klass;
+    
+    public int getKlass() {
+        return klass;
+    }
+
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        // TODO Auto-generated constructor stub
+        this.klass=klass;
+    }
+    
+
+    @Override
+    public String introduce() {
+        // TODO Auto-generated method stub
+        return super.introduce()+" "+"I am a Student. I am at Class "+klass+".";
+    }
 }
