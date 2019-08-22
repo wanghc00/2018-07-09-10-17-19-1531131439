9a2b8a2a04aa7684b9b9567fce0368e5d301c6fe
wanghc00 <306093226@qq.com>
2019-08-22 22:09:19 +0800

    practice03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,18 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+
+    public Worker(String name, int age) {
+        super(name, age);
+        // TODO Auto-generated constructor stub
+    }
+    
+    /* (non-Javadoc)
+     * @see practice03.Person#introduce()
+     */
+    @Override
+    public String introduce() {
+        // TODO Auto-generated method stub
+        return "I am a Worker. I have a job.";
+    }
 }
